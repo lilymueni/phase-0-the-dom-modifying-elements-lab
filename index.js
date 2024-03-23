850d41dd518181856cc3eae2e25226921c8c31b9
lilianmueninzioki <liliannzioki@gmail.com>
2024-03-23 10:33:01 +0300

    DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const newHeader = document.querySelector('h1');
+
+document.addEventListener('DOMContentLoaded', () => {
+    var newHeader = document.getElementById('victory');
+    newHeader.textContent = 'LILY-NZIOKI is the champion';
+});
